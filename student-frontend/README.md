ccc3ba6e25a904fe8bb040089fdb6c1b5032de3e
kauanzin222 <kauaanthony.cardoso44@gmail.com>
2025-11-07 19:03:53 -0300

    closes #1 - adicionando Cadastro de Alunos a pasta frontend

---
+++ student-frontend/README.md
@@ -0,0 +1,14 @@
+# BootCamp - Module 5 Student Registration 🚀
+Project for module progression. Improving HTML, JavaScript &amp; BootStrap. The project must follow the following example images:
+  - [Desktop Version](https://github.com/kauanzin222/bootcamp-devjr-modulo5-cadastro-aluno/blob/main/imagesEx/desktop.png) 🖥️
+  - [Mobile Version](https://github.com/kauanzin222/bootcamp-devjr-modulo5-cadastro-aluno/blob/main/imagesEx/mobile.png) 📱
+
+
+--- 
+## Final Result: 
+
+### - Desktop: 
+![desktop_version](images/desktopResult.png) 
+
+### - Mobile: 
+![mobile_version](images/mobileResult.png)
---
+++ student-frontend/js/script.js
@@ -0,0 +1,104 @@
+$('#inputPhone').mask('(00) 00000-0000');
+
+var courses = [
+    { id: 1, name: "Java" },
+    { id: 2, name: "Python" },
+    { id: 3, name: "C#" },
+];
+
+var turnos = [
+    { id: "1", name: "Manhã" },
+    { id: "2", name: "Tarde" },
+    { id: "3", name: "Noturno" },
+];
+
+var alunos = [];
+
+function save() {
+
+    /* Colocando os valores em um vetor */
+    var aluno =
+    {
+        id: alunos.length + 1,
+        name: document.getElementById("inputName").value,
+        email: document.getElementById("inputEmail").value,
+        phone: document.getElementById("inputPhone").value,
+        course: document.getElementById("selectCourse").value,
+        turno: getRadioValue(),
+    }
+
+
+    addNewRow(aluno);
+
+    alunos.push(aluno);
+
+    document.getElementById("formAluno").reset();
+
+    console.log(aluno);
+};
+
+function getRadioValue() {
+    radios = document.getElementsByName("gridRadios");
+
+    for (let radio of radios)
+        if (radio.checked)
+            return radio.value;
+};
+
+function addNewRow(aluno) {
+    var table = document.getElementById("tableAlunos");
+
+    var newRow = table.insertRow();
+
+    // insert id aluno
+    var idNode = document.createTextNode(aluno.id);
+    newRow.insertCell().appendChild(idNode);
+
+    // insert name aluno
+    var nameNode = document.createTextNode(aluno.name);
+    newRow.insertCell().appendChild(nameNode);
+
+    // insert email aluno
+    var emailNode = document.createTextNode(aluno.email);
+
+    var cell = newRow.insertCell();
+    cell.className = "d-none d-md-table-cell";
+
+    cell.appendChild(emailNode);
+    
+    // insert phone aluno
+    var phoneNode = document.createTextNode(aluno.phone);
+
+    cell = newRow.insertCell();
+    cell.className = "d-none d-md-table-cell";
+
+    cell.appendChild(phoneNode);
+
+    // insert course aluno 
+    for (let course of courses) {
+        if (course.id == aluno.course) {
+            var courseNode = document.createTextNode(course.name);
+            break;
+        }
+    }
+
+    newRow.insertCell().appendChild(courseNode);
+
+
+    // insert shift aluno 
+    for (let turno of turnos) {
+        console.log(
+            "Comparando:", turno.id, "(tipo:", typeof turno.id, ")",
+            "com:", aluno.turno, "(tipo:", typeof aluno.turno, ")"
+        );
+        if (turno.id === aluno.turno) {
+            var turnoNode = document.createTextNode(turno.name);
+            break;
+        }
+    }
+
+    cell = newRow.insertCell();
+    cell.className = "d-none d-md-table-cell";
+
+    cell.appendChild(turnoNode);
+}
